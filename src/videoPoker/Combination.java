ec082bfc3a60c08e3646f610e22cd56ac62a25d8
sebdd <sebden00@gmail.com>
2019-12-02 15:52:00 +0100

    Uppdaterar enum lägger till värde på olika kombinationerna

--- src/videoPoker/Combination.java
+++ src/videoPoker/Combination.java
@@ -2,15 +2,19 @@ package videoPoker;
 
 public enum Combination {
 	
-	RoyalFlush,
-	StraightFlush,
-	FourOfaKind,
-	FullHouse,
-	Flush,
-	Straight,
-	ThreeOfaKind,
-	TwoPair,
-	PairOfJacks;
+	RoyalFlush(50),
+	StraightFlush(25),
+	FourOfaKind(25),
+	FullHouse(9),
+	Flush(6),
+	Straight(4),
+	ThreeOfaKind(3),
+	TwoPair(2),
+	PairOfJacks(1);
 	
+	public final int value;
+		private Combination(int value) {
+			this.value = value;
+		}
 
 }

fa0c37244c7c92f7750d1d395120b9bf222d77f9
Daniljela <daijela.blace@gmail.com>
2019-12-04 15:51:32 +0100

    byter namn av file från Test till tester. Hade problem pga namn

---
+++ src/videoPoker/Tester.java
@@ -0,0 +1,16 @@
+package videoPoker;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class Tester {
+	
+	
+	@Test
+	void femKort()
+	{}
+
+}

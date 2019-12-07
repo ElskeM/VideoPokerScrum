4d4b57229e1c10b4e5c36a983bcd6eda8df64321
Daniljela <daijela.blace@gmail.com>
2019-12-07 22:24:25 +0100

    Show dialog funkar inte,med Label spelare kan alltid titta på regelna

--- src/videoPoker/Main.java
+++ src/videoPoker/Main.java
@@ -1,82 +1,109 @@
 package videoPoker;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
-
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		System.out.println("Välkommen till VideoPoker! Du har " + new Readfile().readfile() + " krediter");
-		String regler =   "***************************************************************************************************\n"
-						+ "       Spelregler                                                                                  \n"
-						+ "	      Du kan byta hur många kort du vill, men bara en gång 								          \n"
-						+ "                                                                                                   \n"
-						+ "    9: ROYAL FLUSH                -- Tio, knekt, dam, kung och ess i samma kortsvit          250x  \n"
-						+ "    8: FÄRGSTEGE                  -- Straight Flush - Fem kort i rad i samma kortsvit         50x  \n"
-						+ "    7: FYRTAL                  	 -- Fyra kort med samma valör                                25x  \n"
-						+ "    6: KÅK                    	 -- Ett par och en triss                                      9x  \n"
-						+ "    5: FÄRG                   	 -- Fem kort med samma kortsvit                               6x  \n"
-						+ "    4: STEGE                  	 -- Fem kort i rad                                            4x  \n"
-						+ "    3: TRISS                  	 -- Tre kort med samma valör                                  3x  \n"
-						+ "    1: PAR                    	 -- Två knektar eller bättre                                  1x  \n"
-						+ "***************************************************************************************************\n";
 
-
-JOptionPane.showMessageDialog(null, regler);
-
+		Regel regel = new Regel();
 
 		System.out.println();
 
+		String s;
+		int choice = 0;
+		boolean loop = true;
+		Scanner in = new Scanner(System.in);
 
-		        String s;
-		        int choice = 0;
-		        boolean loop = true;
-		        Scanner in = new Scanner(System.in);
+		do {
+			System.out.println("|*********************************|\n" + "|    Vad vill du göra?            |\n"
+					+ "|    1: Spela med konto           |\n" + "|    2: Spela utan konto          |\n"
+					+ "|    0: Quit                      |\n" + "|*********************************|");
 
-		        do {
-				System.out.println("|*********************************|\n"
-								 + "|    Vad vill du göra?            |\n"
-								 + "|    1: Spela med konto           |\n"
-								 + "|    2: Spela utan konto          |\n"
-								  +"|    0: Quit                      |\n"
-								 + "|*********************************|");
+			s = in.nextLine();
 
+			try {
+				choice = Integer.parseInt(s);
+				loop = false;
+			} catch (Exception e) {
+				System.out.println("Felaktig inmatning! Endast tal!");
+			}
+			VideoPoker videoPoker = new VideoPoker();
+			switch (choice) {
+			case 1:
+				int credit = Integer.parseInt(new Readfile().readfile());
+				videoPoker.setCredit(credit);
+				videoPoker.changeCards();
+				break;
+			case 2:
+				videoPoker = new VideoPoker();
+				break;
 
-		            s = in.nextLine();
+			case 0:
+				new WriteFile().writeFile(videoPoker.getCredit());
+				System.out.println("Tack för att du spelade Video Poker!");
+				System.out.println("Vi ses nästa gång!");
+				in.close();
+				break;
+			default:
+				System.out.println("Felaktig inmatning!");
+				break;
+			}
 
-		            try {
-		                choice = Integer.parseInt(s);
-		                loop = false;
-		            }
-		            catch (Exception e)
-		            {
-		                System.out.println("Felaktig inmatning! Endast tal!");
-		            }
-		            VideoPoker videoPoker= new VideoPoker() ;
-		            switch (choice) {
-		            case 1:
-		            	int credit = Integer.parseInt(new Readfile().readfile());
-		            	videoPoker.setCredit(credit);
-		                videoPoker.changeCards();
-		                break;
-		            case 2:
-		            	videoPoker= new VideoPoker() ;
-		                break;
-
-		            case 0:
-		            	new WriteFile().writeFile(videoPoker.getCredit());
-		                System.out.println("Tack för att du spelade Video Poker!");
-		                System.out.println("Vi ses nästa gång!");
-		                in.close();
-		                break;
-		            default:
-		                    System.out.println("Felaktig inmatning!");
-		                    break;
-		            }
-
-		        } while (choice != 0);
+		} while (choice != 0);
 
 	}
+}
+
+class Regel extends JFrame {
+	public Regel() {// konstruktor ,anropas automatisk
+
+//regel är nu i Label så spelare kan alltid titta på regler
+		JLabel reg = new JLabel("<html><body><table border='0'>" + "<tr>"
+				+ "<td>*****************************************************************************</td>" + "</tr>"
+				+ "<tr>" + "<td></td><td>Spelregler </td>" + "</tr>" + "<tr>"
+				+ "<td></td><td>Du kan byta hur många kort du vill, men bara en gång </td>	" + "</tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>"
+				+ "<td>9: ROYAL FLUSH </td><td>Tio, knekt, dam, kung och ess i samma kortsvit</td><td>250x</td>"
+				+ " </tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>8: FÄRGSTEGE </td><td>Straight Flush - Fem kort i rad i samma kortsvit</td><td>50x</td>"
+				+ "</tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>7: FYRTAL</td><td>Fyra kort med samma valör</td><td>25x </td>" + "</tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>6: KÅK</td><td>Ett par och en triss</td><td> 9x</td>" + "</tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>5: FÄRG </td><td>Fem kort med samma kortsvit</td><td>6x</td>" + "</tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>4: STEGE </td><td>Fem kort i rad</td><td>4x</td>" + "</tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>3: TRISS</td><td>Tre kort med samma valör</td>3x<td>" + " </tr>"
+				+ "<tr>--------------------------------------------------------------------------------------------------</tr>"
+				+ "<tr>" + "<td>1: PAR</td><td>Två knektar eller bättre </td><td>1x<td>" + " </tr>"
+				+ "<tr>*****************************************************************************"
+
+				+ "</tr>" + "</table></body></html>", JLabel.CENTER);
+
+		add(reg);// placera reg i fönster
+		reg.setOpaque(true);
+		reg.setBackground(Color.GREEN);
+		reg.setForeground(Color.BLACK);
+		reg.setFont((new Font("SansSerif", Font.ITALIC, 12)));
+
+		setSize(900, 600);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	}
+
 }
